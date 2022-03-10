b632c82f1f3c69d512c5f04b2522a0e17dd653c2
Yunus Irmak <yunusirmak75@hotmail.com>
2022-03-10 15:07:24 +0300

    updated routues

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,22 +10,16 @@ export default function Router() {
   const user = JSON.parse(localStorage.getItem("user")) || null;
   return useRoutes([
     {
-      path: "/dashboard",
+      path: "/dashboard/",
       element: <Dashboard />,
-      children: [
-        { path: "yunus", element: <Dashboard /> },
-        { path: "app", element: <Users /> },
-      ],
+      children: [{ path: "app", element: <Users /> }],
     },
     {
       path: "/",
-      element: user !== null ? <Navigate to="/dashboard" /> : <HomePage />,
+      element: user !== null ? <Navigate to="/dashboard/app" /> : <HomePage />,
       children: [
         { path: "/", element: <Navigate to="/dashboard/app" /> },
-        { path: "login", element: <HomePage /> },
-        { path: "register", element: <HomePage /> },
-        { path: "404", element: <Dashboard /> },
-        { path: "*", element: <Navigate to="/404" /> },
+        { path: "*", element: <Navigate to="/" /> },
       ],
     },
     { path: "*", element: <Navigate to="/404" replace /> },
